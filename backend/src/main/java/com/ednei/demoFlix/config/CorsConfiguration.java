78944c9fa0f1989ab7503ac9bb17be4f7448e032
Ednei Gonzaga <edneigonzaga444@gmail.com>
2025-11-26 18:07:16 -0300

    implementando url local para testes CorsConfiguration

--- backend/src/main/java/com/ednei/demoFlix/config/CorsConfiguration.java
+++ backend/src/main/java/com/ednei/demoFlix/config/CorsConfiguration.java
@@ -7,7 +7,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://demoflix3.netlify.app")
+                .allowedOrigins("https://demoflix3.netlify.app", "http://127.0.0.1:5501")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD");
     }
 }
